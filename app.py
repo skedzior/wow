93a940b58569c57acf55e3a44c2205f380af6906
Steven Kedzior <stevenkedzior@gmail.com>
2021-02-20 20:43:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,18 +21,3 @@ def get_balances():
             ff = b['free']
     return ff
 
-@app.route("/wallet")
-def get_wallet_info():
-    client = ftx.FtxClient('mt6q3o7aheFH89_GMdZUZXf8JbLfJ8oynNRWOIbM','inl1LzA10irNDSb17wABqwFVot0mkx84QumaAsiL', 'test')
-    return {'wallet':client.get_wallet_info()}
-
-@app.route("/balances")
-def get_balances():
-    client = ftx.FtxClient('mt6q3o7aheFH89_GMdZUZXf8JbLfJ8oynNRWOIbM','inl1LzA10irNDSb17wABqwFVot0mkx84QumaAsiL', 'test')
-    return {'balances':client.get_balances()}
-
-@app.route("/futures")
-def get_futures():
-    client = ftx.FtxClient('mt6q3o7aheFH89_GMdZUZXf8JbLfJ8oynNRWOIbM','inl1LzA10irNDSb17wABqwFVot0mkx84QumaAsiL', 'test')
-    return {'futures':client.get_futures()}
-
