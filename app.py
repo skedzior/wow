1da49395f1572d889bfe26f9c4ba03541bbdfdcf
Steven Kedzior <stevenkedzior@gmail.com>
2021-02-20 19:42:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,21 +6,24 @@ import ftx
 
 app = Flask("__main__")
 
-client = ftx.FtxClient('mt6q3o7aheFH89_GMdZUZXf8JbLfJ8oynNRWOIbM','inl1LzA10irNDSb17wABqwFVot0mkx84QumaAsiL', 'test')
 
 @app.route("/positions")
 def get_positions():
+    client = ftx.FtxClient('mt6q3o7aheFH89_GMdZUZXf8JbLfJ8oynNRWOIbM','inl1LzA10irNDSb17wABqwFVot0mkx84QumaAsiL', 'test')
     return {'positions':client.get_positions()}
 
 @app.route("/wallet")
 def get_wallet_info():
+    client = ftx.FtxClient('mt6q3o7aheFH89_GMdZUZXf8JbLfJ8oynNRWOIbM','inl1LzA10irNDSb17wABqwFVot0mkx84QumaAsiL', 'test')
     return {'wallet':client.get_wallet_info()}
 
 @app.route("/balances")
 def get_balances():
+    client = ftx.FtxClient('mt6q3o7aheFH89_GMdZUZXf8JbLfJ8oynNRWOIbM','inl1LzA10irNDSb17wABqwFVot0mkx84QumaAsiL', 'test')
     return {'balances':client.get_balances()}
 
 @app.route("/futures")
 def get_futures():
+    client = ftx.FtxClient('mt6q3o7aheFH89_GMdZUZXf8JbLfJ8oynNRWOIbM','inl1LzA10irNDSb17wABqwFVot0mkx84QumaAsiL', 'test')
     return {'futures':client.get_futures()}
 
