dc087276c7f9c70eddb14f5b225dcc2a9579b540
Steven Kedzior <stevenkedzior@gmail.com>
2021-02-20 21:32:16 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+WEBHOOK_PASSPHRASE = "yourpassphrase"
+
+API_KEY = 'MeFt30cxXobaAKzwl01kvcK49Tkee8jvCrUejRcmS6tdMJxZYba6rP2dl5qNlORS'
+API_SECRET = 'iA2jbTAxtjTZaxUf1EEGw0eH4hKhA8nw2HDyL6yPeDOVM0BfYigstItGoxqLioUy'
