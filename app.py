6f63ed499807edeec77e0a357be862f625a67d31
Steven Kedzior <stevenkedzior@gmail.com>
2021-02-20 21:30:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,55 @@
 import json
 from flask import Flask, request, jsonify
-import ftx
-
+from binance.client import Client
+from binance.enums import *
 
 
 app = Flask("__main__")
+
+client = Client(config.API_KEY, config.API_SECRET, tld='us')
 
 @app.route("/")
 def hello():
     return 'helol'
 
-@app.route("/webhook", methods=['POST'])
+def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
+    try:
+        print(f"sending order {order_type} - {side} {quantity} {symbol}")
+        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+    except Exception as e:
+        print("an exception occured - {}".format(e))
+        return False
+
+    return order
+
+@app.route('/')
+def welcome():
+    return render_template('index.html')
+
+@app.route('/webhook', methods=['POST'])
 def webhook():
-    client = ftx.FtxClient('mt6q3o7aheFH89_GMdZUZXf8JbLfJ8oynNRWOIbM','inl1LzA10irNDSb17wABqwFVot0mkx84QumaAsiL', 'test')
-    client.place_order('ETH/USD', 'sell', None, 0.005, 'market')
-    return 'sell'
+    #print(request.data)
+    data = json.loads(request.data)
+    
+    if data['passphrase'] != config.WEBHOOK_PASSPHRASE:
+        return {
+            "code": "error",
+            "message": "Nice try, invalid passphrase"
+        }
 
+    side = data['strategy']['order_action'].upper()
+    quantity = data['strategy']['order_contracts']
+    order_response = order(side, quantity, "DOGEUSD")
 
+    if order_response:
+        return {
+            "code": "success",
+            "message": "order executed"
+        }
+    else:
+        print("order failed")
+
+        return {
+            "code": "error",
+            "message": "order failed"
+        }
