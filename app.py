8f199d8742d849dbd96f92410509691620a42ce5
Steven Kedzior <stevenkedzior@gmail.com>
2021-02-20 21:19:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ import ftx
 
 app = Flask("__main__")
 
-client = ftx.FtxClient('mt6q3o7aheFH89_GMdZUZXf8JbLfJ8oynNRWOIbM','inl1LzA10irNDSb17wABqwFVot0mkx84QumaAsiL', 'test')
-
 @app.route("/")
 def hello():
     return 'helol'
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
+    client = ftx.FtxClient('mt6q3o7aheFH89_GMdZUZXf8JbLfJ8oynNRWOIbM','inl1LzA10irNDSb17wABqwFVot0mkx84QumaAsiL', 'test')
     client.place_order('ETH/USD', 'sell', None, 0.005, 'market')
     return 'sell'
 
